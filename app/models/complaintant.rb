1e614fc26924e81b0740ba1ee06d398efcc6a940
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-10-05 15:04:56 -0400

    relationships

--- app/models/complaintant.rb
+++ app/models/complaintant.rb
@@ -1,2 +1,5 @@
 class Complaintant < ApplicationRecord
+    has_many :incidents
+    belongs_to :managers
+    has_many :aggressors, through: :incidents
 end
--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,2 +1,5 @@
 class Manager < ApplicationRecord
+    belongs_to :company
+    has_many :complaintants
+    has_many :incidents, through: :complaintants
 end
